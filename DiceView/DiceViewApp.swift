8b5b3b2c1e979c0a14079992599e410c3da7dc05
Jeremy Kim <jedo@alumni.unc.edu>
2025-04-27 15:03:41 -0400

    Initial Commit

---
+++ DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Jeremy Kim on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
